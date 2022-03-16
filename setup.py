78a1f331f8b5ffbd520e1ac9fb1083a24e92906e
aloctavodia <aloctavodia@gmail.com>
2022-03-16 18:25:11 +0200

    update python support

--- setup.py
+++ setup.py
@@ -80,6 +80,6 @@ if __name__ == "__main__":
         # package_data={'docs': ['*']},
         include_package_data=True,
         classifiers=classifiers,
-        python_requires=">=3.7",
+        python_requires=">=3.8",
         install_requires=install_reqs,
     )
