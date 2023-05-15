c42cba4d29d275ba5b1261c9af24bab9e73a270d
Oriol Abril-Pla <oriol.abril.pla@gmail.com>
2023-05-15 21:44:35 +0200

    Add section targets to gallery grid page (#546)
    * Update thumbnail_extractor.py
    
    * black

--- sphinxext/thumbnail_extractor.py
+++ sphinxext/thumbnail_extractor.py
@@ -77,6 +77,8 @@ Core notebooks
 """
 
 SECTION_TEMPLATE = """
+.. _{section_id}:
+
 {section_title}
 {underlines}
 
@@ -184,7 +186,11 @@ def main(app):
     for folder, title in folder_title_map.items():
 
         nb_paths = glob(f"{folder}/*.ipynb")
-        file.append(SECTION_TEMPLATE.format(section_title=title, underlines="-" * len(title)))
+        file.append(
+            SECTION_TEMPLATE.format(
+                section_title=title, section_id=folder, underlines="-" * len(title)
+            )
+        )
         target_dir = os.path.join("..", "_thumbnails", folder)
         if not os.path.isdir(target_dir):
             os.mkdir(target_dir)
